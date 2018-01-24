6670b121f55e6fe2339094e496f0dc248c38c6a1
Josh Kendall <jkendall@celebratedrupal.com>
2018-01-23 17:04:46 -0800

    Add source maps for css->sass

--- web/themes/custom/durhamtech/Gulpfile.js
+++ web/themes/custom/durhamtech/Gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function() {
   gulp.src('scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./css/'))
 });
 
