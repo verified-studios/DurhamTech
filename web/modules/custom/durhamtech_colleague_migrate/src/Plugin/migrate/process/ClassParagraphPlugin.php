e30691235ef8151031946fb5ac234932c4e8b0c6
Chris McGrath <chris@verifiedstudios.com>
2018-03-12 16:42:25 +0200

    Dtech migration - Class data. Fix data in Paragraphs

--- web/modules/custom/durhamtech_colleague_migrate/src/Plugin/migrate/process/ClassParagraphPlugin.php
+++ web/modules/custom/durhamtech_colleague_migrate/src/Plugin/migrate/process/ClassParagraphPlugin.php
@@ -25,6 +25,21 @@ class ClassParagraphPlugin extends ProcessPluginBase {
     $class_data_items = [];
     $source = $row->getSource();
 
+    // Prepare Meeting days data.
+    $days_value = $source['Meeting Days'];
+    $day_rows = explode(PHP_EOL, $days_value);
+    $day_rows = array_filter($day_rows, function ($value) {
+      return $value !== '';
+    });
+    foreach ($day_rows as $row_key => $day_row) {
+      if (strpos($day_row, 'ü')) {
+        $class_data_items[$row_key]['field_meeting_days'] = explode('ü', $day_row);
+      }
+      else {
+        $class_data_items[$row_key]['field_meeting_days'] = $day_row;
+      }
+    }
+
     // Prepare rooms data.
     $room_value = $source['Room'];
     if (!is_string($room_value)) {
@@ -35,31 +50,62 @@ class ClassParagraphPlugin extends ProcessPluginBase {
       $class_data_items[$room_key]['field_room'] = $room;
     }
 
-    // Prepare Meeting days data.
-    $days_value = $source['Meeting Days'];
-    $day_rows = explode(PHP_EOL, $days_value);
-    foreach ($day_rows as $row_key => $day_row) {
-      if (strpos($day_row, 'ü')) {
-        $class_data_items[$row_key]['field_meeting_days'] = explode('ü', $day_row);
+    if (!empty($class_data_items[0]['field_room']) && $room_key < $row_key) {
+      for ($i = $room_key + 1; $i <= $row_key; $i++) {
+        $class_data_items[$i]['field_room'] = $room;
       }
-      else {
-        $class_data_items[$row_key]['field_meeting_days'] = $day_row;
+    }
+
+    if (!empty($class_data_items[0]['field_meeting_days']) && $row_key < $room_key) {
+      for ($i = $row_key + 1; $i <= $room_key; $i++) {
+        $class_data_items[$i]['field_meeting_days'] = $class_data_items[0]['field_meeting_days'];
       }
     }
 
     // Prepare Start Time.
     $start_time_value = $source['Start Time'];
     $start_time_row = explode(PHP_EOL, $start_time_value);
+    $start_time_row = array_filter($start_time_row, function ($value) {
+      return $value !== '';
+    });
     foreach ($start_time_row as $start_time_key => $start_time) {
       $class_data_items[$start_time_key]['field_start_time'] = $start_time;
+    }
+
+    if (!empty($class_data_items[0]['field_start_time']) && $start_time_key < $row_key) {
+      for ($i = $start_time_key + 1; $i <= $row_key; $i++) {
+        $class_data_items[$i]['field_start_time'] = $start_time;
+      }
+    }
+
+    if (!empty($class_data_items[0]['field_start_time']) && $start_time_key < $room_key) {
+      for ($i = $start_time_key + 1; $i <= $room_key; $i++) {
+        $class_data_items[$i]['field_start_time'] = $start_time;
+      }
     }
 
     // Prepare End Time.
     $end_time_value = $source['End Time'];
     $end_time_row = explode(PHP_EOL, $end_time_value);
+    $end_time_row = array_filter($end_time_row, function ($value) {
+      return $value !== '';
+    });
     foreach ($end_time_row as $end_time_key => $end_time) {
       $class_data_items[$end_time_key]['field_end_time'] = $end_time;
     }
+
+    if (!empty($class_data_items[0]['field_end_time']) && $end_time_key < $row_key) {
+      for ($i = $end_time_key + 1; $i <= $row_key; $i++) {
+        $class_data_items[$i]['field_end_time'] = $end_time;
+      }
+    }
+
+    if (!empty($class_data_items[0]['field_end_time']) && $end_time_key < $room_key) {
+      for ($i = $end_time_key + 1; $i <= $room_key; $i++) {
+        $class_data_items[$i]['field_end_time'] = $end_time;
+      }
+    }
+
 
     // Get node Id.
     $nid = $row->getIdMap()['destid1'];
