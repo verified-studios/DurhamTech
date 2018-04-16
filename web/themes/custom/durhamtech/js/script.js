f4cafda576b94a593d590574539499dc269cd9e3
Joshua Kendall <jkendall@celebratedrupal.com>
2018-04-16 11:05:52 -0700

    Add search bar theming

--- web/themes/custom/durhamtech/js/script.js
+++ web/themes/custom/durhamtech/js/script.js
@@ -4,4 +4,13 @@
 
     }
   };
+
+  Drupal.behaviors.header = {
+    attach: function (context, settings) {
+      // Open/close search
+      $('.navbar-collapse .js-search-trigger').once().click(function() {
+        $('.block-views-exposed-filter-blocksearch-page-1').toggle();
+      });
+    }
+  };
 })(jQuery, Drupal);
