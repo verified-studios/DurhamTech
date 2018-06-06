4f0c61723ed6e3bdac8cf8142fae3a577af575d4
Aaron Andy <aaron@verifiedstudios.com>
2018-06-06 15:11:10 -0400

    social slider

--- web/modules/custom/durhamtech_social/src/Plugin/Block/SocialBlock.php
+++ web/modules/custom/durhamtech_social/src/Plugin/Block/SocialBlock.php
@@ -29,12 +29,30 @@ class SocialBlock extends BlockBase
             ),
             '#markup' => '
             <div class="slick">
-                <div><img src="sites/default/files/2018-03/782849_c0fae58e.jpg"/></div>
-                <div><img src="sites/default/files/2018-05/action-2277292_640.jpg"/></div>
-                <div><img src="sites/default/files/2018-05/DT Engineering THUMBNAIL_1.jpg"/></div>
-                <div><img src="sites/default/files/2018-05/golden leaf_0.jpg"/></div>
-                <div><img src="sites/default/files/2018-05/momentum THUMBNAIL.jpg"/></div>
-                <div><img src="sites/default/files/2018-05/DT Engineering THUMBNAIL_1.jpg"/></div>
+                <div>
+                    <img src="sites/default/files/2018-03/782849_c0fae58e.jpg"/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
+                </div>
+                <div>
+                    <img src="sites/default/files/2018-05/action-2277292_640.jpg"/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
+                </div>
+                <div>
+                    <img src="sites/default/files/2018-05/DT Engineering THUMBNAIL_1.jpg"/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
+                </div>
+                <div>
+                <img src="sites/default/files/2018-05/golden leaf_0.jpg"/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
+                </div>
+                <div>
+                <img src="sites/default/files/2018-05/momentum THUMBNAIL.jpg"/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
+                </div>
+                <div>
+                <img src="sites/default/files/2018-05/DT Engineering THUMBNAIL_1.jpg"/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
+                </div>
             </div>
             ',
         );
--- web/themes/custom/durhamtech/js/script.js
+++ web/themes/custom/durhamtech/js/script.js
@@ -38,32 +38,32 @@
         $('.slick').slick({
             infinite: true,
             speed: 300,
-            slidesToShow: 3,
+            slidesToShow: 5,
             slidesToScroll: 1,
             arrows: true,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
+            // responsive: [
+            //     {
+            //         breakpoint: 1024,
+            //         settings: {
+            //             slidesToShow: 3,
+            //             slidesToScroll: 1,
+            //         }
+            //     },
+            //     {
+            //         breakpoint: 600,
+            //         settings: {
+            //             slidesToShow: 2,
+            //             slidesToScroll: 2
+            //         }
+            //     },
+            //     {
+            //         breakpoint: 480,
+            //         settings: {
+            //             slidesToShow: 1,
+            //             slidesToScroll: 1
+            //         }
+            //     }
+            // ]
         });
 
     }
