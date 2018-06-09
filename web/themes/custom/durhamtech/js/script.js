3e1895ebacaa50fc554c26079eb2387cd60ea715
Aaron Andy <aaron@verifiedstudios.com>
2018-06-08 20:09:55 -0400

    slick

--- web/themes/custom/durhamtech/js/script.js
+++ web/themes/custom/durhamtech/js/script.js
@@ -41,29 +41,6 @@
             slidesToShow: 5,
             slidesToScroll: 1,
             arrows: true,
-            // responsive: [
-            //     {
-            //         breakpoint: 1024,
-            //         settings: {
-            //             slidesToShow: 3,
-            //             slidesToScroll: 1,
-            //         }
-            //     },
-            //     {
-            //         breakpoint: 600,
-            //         settings: {
-            //             slidesToShow: 2,
-            //             slidesToScroll: 2
-            //         }
-            //     },
-            //     {
-            //         breakpoint: 480,
-            //         settings: {
-            //             slidesToShow: 1,
-            //             slidesToScroll: 1
-            //         }
-            //     }
-            // ]
         });
 
     }
