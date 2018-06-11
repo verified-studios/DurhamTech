fd418e9de6bbbf7a3ea538f7de09e2a89d174a3b
Mat Jordan <mathew@verifiedstudios.com>
2018-06-11 17:19:43 -0400

    twitter/instagram on social

--- web/modules/custom/durhamtech_social/src/Plugin/Block/SocialBlock.php
+++ web/modules/custom/durhamtech_social/src/Plugin/Block/SocialBlock.php
@@ -18,8 +18,23 @@ class SocialBlock extends BlockBase
     /**
      * {@inheritdoc}
      */
+
     public function build()
     {
+        $output = '';
+
+        $instagram  = Instagram::getPosts(4);
+        $twitter    = Twitter::getPosts(4);
+        $posts      = array_merge($instagram, $twitter);
+
+        shuffle($posts);
+
+        foreach ($posts as $k => $post ) {
+            $output .= $post;
+        }
+
+        $slick = '<div class="slick">' . $output . '</div>';
+
         return array(
             '#type' => 'markup',
             '#attached' => array(
@@ -27,34 +42,96 @@ class SocialBlock extends BlockBase
                     'durhamtech_social/slick'
                 ),
             ),
-            '#markup' => '
-            <div class="slick">
-                <div>
-                    <img src="sites/default/files/2018-03/782849_c0fae58e.jpg"/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
-                </div>
-                <div>
-                    <img src="sites/default/files/2018-05/action-2277292_640.jpg"/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
-                </div>
-                <div>
-                    <img src="sites/default/files/2018-05/DT Engineering THUMBNAIL_1.jpg"/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
-                </div>
-                <div>
-                <img src="sites/default/files/2018-05/golden leaf_0.jpg"/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
-                </div>
-                <div>
-                <img src="sites/default/files/2018-05/momentum THUMBNAIL.jpg"/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
-                </div>
-                <div>
-                <img src="sites/default/files/2018-05/DT Engineering THUMBNAIL_1.jpg"/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis sollicitudin erat. Praesent consectetur ornare sodales. Phasellus tempor arcu lacinia, egestas purus quis, pretium justo</p>
-                </div>
-            </div>
-            ',
+            '#markup' => $slick,
         );
+    }
+}
+
+class Instagram {
+    public static function getPosts($num) {
+        $endpoint = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=1918069938.51b3acd.74343440f35d49fe9d2500404b60a681';
+        $json = file_get_contents($endpoint);
+        $obj = json_decode($json);
+        $count = 0;
+        $items = array();
+        while ($num > $count) {
+            $thumb = $obj->data[$count]->images->standard_resolution->url;
+            $caption = $obj->data[$count]->caption->text;
+            $items[$count] = '<div class="social-slide social-instagram"><img src="' . $thumb .'" alt="'. $caption .'" /><p>' . $caption .'</p><div class="social-icon"><span class="fa fa-instagram"></span></div></div>';
+            $count++;
+        }
+        return $items;
+    }
+}
+
+class Twitter {
+
+    public static function buildBaseString($baseURI, $method, $params) {
+        $r = array();
+        ksort($params);
+        foreach($params as $key=>$value){
+            $r[] = "$key=" . rawurlencode($value);
+        }
+        return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+    }
+
+    public static function buildAuthorizationHeader($oauth) {
+        $r = 'Authorization: OAuth ';
+        $values = array();
+        foreach($oauth as $key=>$value)
+            $values[] = "$key=\"" . rawurlencode($value) . "\"";
+        $r .= implode(', ', $values);
+        return $r;
+    }
+
+    public static function getPosts($num) {
+
+        $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+        $oauth_access_token = "37061935-WVrr4evWBGtaEtwjQe8qB35qL36H0dJAUmKQNJmsA";
+        $oauth_access_token_secret = "HbuRLIq2bGFiBhrUnIqEGmGeghcSSxB68w6MSHFLESeEa";
+        $consumer_key = "DMJRJjROI4IbVR2JK2PMqlBo3";
+        $consumer_secret = "sLnlYxH7gvcgkT7fBjb0WtQsK5Dx74J3cZpEcUeiCOSrSptY76";
+
+        $oauth = array( 'oauth_consumer_key' => $consumer_key,
+            'oauth_nonce' => time(),
+            'oauth_signature_method' => 'HMAC-SHA1',
+            'oauth_token' => $oauth_access_token,
+            'oauth_timestamp' => time(),
+            'oauth_version' => '1.0');
+
+        $base_info = Twitter::buildBaseString($url, 'GET', $oauth);
+        $composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
+        $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+        $oauth['oauth_signature'] = $oauth_signature;
+
+        // Make requests
+        $header = array(Twitter::buildAuthorizationHeader($oauth), 'Expect:');
+        $options = [
+            CURLOPT_HTTPHEADER => $header,
+            CURLOPT_HEADER => false,
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false
+        ];
+
+        $feed = curl_init();
+        curl_setopt_array($feed, $options);
+        $json = curl_exec($feed);
+        curl_close($feed);
+
+        $obj = json_decode($json);
+
+
+        $count = 0;
+        $items = array();
+        while ($num > $count) {
+//            $thumb = $obj[$count]->images->standard_resolution->url;
+            $caption = $obj[$count]->text;
+            $items[$count] = '<div class="social-slide social-twitter"><p>' . $caption .'</p><div class="social-icon"><span class="fa fa-twitter"></span></div></div>';
+            $count++;
+        }
+        return $items;
+
     }
 }
