1e3d40d711f4878ae5bd1164b9797ceac3e11cf1
Marc Getter <marc.getter@ibm.com>
2018-11-24 15:57:52 -0500

    Updated programs to use new taxonomy for required courses

--- web/modules/custom/durhamtech_colleague_migrate/src/Plugin/migrate/process/ProgramCourseOutlinePlugin.php
+++ web/modules/custom/durhamtech_colleague_migrate/src/Plugin/migrate/process/ProgramCourseOutlinePlugin.php
@@ -20,14 +20,27 @@ class ProgramCourseOutlinePlugin extends ProcessPluginBase {
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     $course_outline_data_items = [];
-    $terms = \Drupal::entityTypeManager()
-      ->getStorage('taxonomy_term')
-      ->loadTree('course');
+    //$value_array = explode(',', $value);
+    $taxonomyStorage = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term');
+    $courses = $taxonomyStorage->loadTree('course');
 
-    if (!empty($terms)) {
-      foreach ($terms as $term) {
-        if (strripos($value, $term->name)) {
-          $course_outline_data_items[$term->tid][] = $term->tid;
+    if (!empty($courses)) {
+      $programs_course_taxonomy_entry = \Drupal::entityQuery('taxonomy_term')
+        ->condition('vid', 'programs_course_list')
+        ->condition('field_program_id', $value)
+        ->execute();
+      if (!empty($programs_course_taxonomy_entry)) {
+        $programs_course_taxonomy_entry_term_id = key($programs_course_taxonomy_entry);
+        $program_courses = $taxonomyStorage->load($programs_course_taxonomy_entry_term_id);
+        if (!empty($program_courses)) {
+          $required_courses = $program_courses->get('field_required_courses')->value;
+          $required_courses_array = explode(',', $required_courses);
+          foreach ($courses as $course) {
+            if (in_array($course->name, $required_courses_array)) {
+              $course_outline_data_items[$course->tid][] = $course->tid;
+            }
+          }
         }
       }
     }
