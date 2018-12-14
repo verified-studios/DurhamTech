4c170b1903d14ff3db6629f02b8009938dc9ebb3
Jamil Lillyreed <jlillyreed@gmail.com>
2018-12-14 17:19:14 -0600

    removing commented code

--- web/themes/custom/durhamtech/js/script.js
+++ web/themes/custom/durhamtech/js/script.js
@@ -33,22 +33,6 @@
 
   Drupal.behaviors.durham_slick = {
     attach: function () {
-      // $('.slick').each(function() {
-      //   var slickIndividual = $(this);
-      //
-      //   slickIndividual.slick({
-      //       infinite: true,
-      //       centerMode: true,
-      //       speed: 500,
-      //       autoplay: true,
-      //       autoplaySpeed: 5000,
-      //       slidesToShow: 3,
-      //       slidesToScroll: 1,
-      //       arrows: true,
-      //       nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><i class="fa fa-arrow-right"></i></button>',
-      //       prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><i class="fa fa-arrow-left"></i></button>',
-      //   })
-      // });
 
       $(".slick-social").not('.slick-initialized').slick({
         infinite: true,
@@ -56,50 +40,43 @@
         speed: 500,
         autoplay: true,
         autoplaySpeed: 5000,
-        //slidesToShow: 1,
-        //slidesToScroll: 1,
         arrows: true,
         nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><i class="fa fa-arrow-right"></i></button>',
         prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><i class="fa fa-arrow-left"></i></button>',
 
         responsive: [
-      {
-        breakpoint: 693,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 99999,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-    ]
+          {
+            breakpoint: 693,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 99999,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
-
-      //$('.slick').slick('reInit', function(slick){
-      //    console.log('slicked');
-      //});
     }
   };
 
   Drupal.behaviors.academicPrograms = {
     attach: function () {
-
-      $( ".apply-tab" ).click(function(e) {
+      $(".apply-tab").click(function (e) {
         e.preventDefault();
         var target = $(this).attr('href');
-        $( ".apply-pane" ).hide();
+        $(".apply-pane").hide();
         $(target).show();
       });
 
@@ -108,20 +85,14 @@
 
   Drupal.behaviors.searchNewsText = {
     attach: function () {
-      $('#views-exposed-form-news-search-block-1 input').once().each(function() {
+      $('#views-exposed-form-news-search-block-1 input').once().each(function () {
         $(this).attr('placeholder', 'Search News')
       });
     }
   };
 
- Drupal.behaviors.selectDateText = {
-   attach: function () {
-     // run function only one time
-     //$('#edit-field-event-date').once().each(function() {
-     //   $(this).attr('placeholder', 'Date (mm/dd/yyyy)')
-             //$(this).val("Select Date");
-     //});
-   }
- };
+  Drupal.behaviors.selectDateText = {
+    attach: function () { }
+  };
 
 })(jQuery, Drupal);
